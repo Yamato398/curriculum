442e660836d3e63e01f1cbb3312c2a2d85579da6
Yamato398 <yiwasawa01hamline@gmail.com>
2021-04-12 18:05:49 +0900

    課題提出テスト
    Check2
    Check.java
    Pet.java
    Constants.java
    以上の作成

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -1,24 +1,31 @@
 package check;
 
+import constants.Constants;
+
+
 public class Check {
 	/**
 	 * 【Check.java】にてフィールド変数firstNameとlastNameを宣言し、
 	 *  firstName →　自分の名字　lastName →　自分の名前で初期化しなさい。
 	 *  なお、変数のアクセス修飾子は「private」とする。
 	 */
-	private String firstName = "Yamato";
-	private String lastName = "Iwasawa";
+	private String firstName = "大和";
+	private String lastName = "岩澤";
 
 	public static void main(String[] args) {
-		
-	}
+		printName("大和", "岩澤");
+		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		pet.introduce();
+		RobotPet robotpet = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		robotpet.introduce();
+	};
 	/**
 	*【Check.java】にてfirstNameとlastNameを引数で受け取って、
 	* 連結して表示させるメソッド「printName」を作成しなさい。
 	* 作成した関数のアクセス修飾子は「private」とする。
 	*/
 	private static void printName(String firstName, String lastName) {
-		String printName = firstName + lastName;
-		System.out.println(printName);
+		String printName = lastName + firstName;
+		System.out.println("printNameメソッド→" + printName);
 	}
 }
